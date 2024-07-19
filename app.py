459d4c9841a7605d0a5a0b89daef96ffde0f159e
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-19 21:41:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.logo("images/Logo.png")
 
 home = st.Page("home.py", title="Home", icon=":material/home:", default=True)
 profile = st.Page("profile.py", title="Profile", icon=":material/person:")
-certificates = st.Page("certificates.py", title="Certificates", icon=":material/list:")
+certificates = st.Page("certificates.py", title="Certificates", icon=":material/workspace_premium:")
 
 pg = st.navigation([profile,home,certificates])
 pg.run()

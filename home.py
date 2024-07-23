57386de76872f2e8770109f78ce9cc352bf4f81b
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-23 16:25:28 +0300

    Update home.py

--- home.py
+++ home.py
@@ -209,7 +209,7 @@ st.write("Freelancing Training Program - Egyptian Ministry of Youth and Sports")
 # st.image("images/Abdallah_Fekry_Mohammed20221019-2047-1pathg3.jpg", caption="Great Learning")
 st.write("---")
 certificates = st.Page("certificates.py")
-if st.button("View Certificates"):
+if st.button("View Certificates", use_container_width=1):
     st.switch_page(certificates)
 
 st.info("Languages")

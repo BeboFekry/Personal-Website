bfb1fd07c1f8fafcffa43bbe2c379e423e87d643
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-30 20:49:21 +0300

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://raw.githubusercontent.com/BeboFekry/ChatHub/main/images/background.jpg");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
+}
+</style>
+"""
+
+st.markdown(background_image, unsafe_allow_html=True)
+
 st.write("---")
 col1, col2 = st.columns([1,3])
 with col1:

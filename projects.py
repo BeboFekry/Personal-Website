b4b17638aa2ab4ebd77fd2dccc161fa8f3b31732
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-01 10:22:35 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -189,7 +189,7 @@ st.divider()
 
 st.info("20. Gender Classification")
 st.write("""Classify Mens and Womans images using Deep Learning - Computer Vision Convolutional Neural Networks CNN Techniques - VGG16 Architecture from data gathering process to deployment process""")
-st.columns([1,3,1])[1].image("images/gender.png")
+st.columns([1,5,1])[1].image("images/gender.png")
 st.link_button("GitHub Repository To See Implementation Notebooks", "https://github.com/BeboFekry/Gender-Images-Classification/blob/main/gender-classification.ipynb", use_container_width=1)
 st.link_button("Kaggle Notebook Link", "https://www.kaggle.com/code/bebofekry/gender-classification", use_container_width=1)
 st.divider()

55884b3c5f2a406be42a3ee85f668724046e6c37
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-06 09:52:03 +0300

    Update certificates.py

--- certificates.py
+++ certificates.py
@@ -60,122 +60,122 @@ st.markdown(background_image, unsafe_allow_html=True)
 st.info("Certificates")
 
 
-st.write("Artificial Intelligence AI and Internet of Things IoT Training - National Telecommunication Institute NTI")
+st.subheader("Artificial Intelligence AI and Internet of Things IoT Training - National Telecommunication Institute NTI")
 st.image("images/NTI AI and IoT Training Certificate.jpg", caption="National Telecommunication Institute NTI")
 st.write("---")
 
-st.write("Internet of Things Connecting Things IoT CT - Cisco Networking Academy")
+st.subheader("Internet of Things Connecting Things IoT CT - Cisco Networking Academy")
 st.image("images/AbdallahFekry Mohammed-IoT CT-certificate_page-0001.jpg", caption="Cisco Networking Academy")
 st.write("---")
 
-st.write("Big Data Essentials Training - National Telecommunication Institute NTI")
+st.subheader("Big Data Essentials Training - National Telecommunication Institute NTI")
 # st.image("images/", caption="National Telecommunication Institute NTI")
 st.write("---")
 
-st.write("Huawei HCIA AI v3.5 - Huawei and the Egyptian Ministry of Youth and Sports")
+st.subheader("Huawei HCIA AI v3.5 - Huawei and the Egyptian Ministry of Youth and Sports")
 # st.image("images/test/", caption="Huawei")
 st.write("---")
 
-st.write("Data Science and Business Analytics Internship Offer Letter - The Sparks Foundation")
+st.subheader("Data Science and Business Analytics Internship Offer Letter - The Sparks Foundation")
 st.image("images/5VET2JCRLM.png", caption="The Sparks Foundation")
 st.write("---")
 
-st.write("Data Science and Business Analytics Internship - The Sparks Foundation")
+st.subheader("Data Science and Business Analytics Internship - The Sparks Foundation")
 st.image("images/MUHCQHJ8JR (1).png", caption="The Sparks Foundation")
 st.write("---")
 
-st.write("Machine Learning Internship Offer Letter - Code Casa")
+st.subheader("Machine Learning Internship Offer Letter - Code Casa")
 st.image("images/Abdallah Fekry Mohammed Offer Letter_page-0001.jpg", caption="Code Casa")
 st.write("---")
 
-st.write("Machine Learning Internship - Code Casa")
+st.subheader("Machine Learning Internship - Code Casa")
 st.image("images/Abdallah Fekry Mohammed Certificate_page-0001.jpg", caption="Code Casa")
 st.write("---")
 
-st.write("Machine Learning Internship Letter of Recommendation - Code Casa")
+st.subheader("Machine Learning Internship Letter of Recommendation - Code Casa")
 st.image("images/Letter of Recommendation (Code Casa).jpg", caption="Code Casa")
 st.write("---")
 
-st.write("Python Basic - Hacker Rank")
+st.subheader("Python Basic - Hacker Rank")
 st.image("images/python basic.jpg", caption="Hacker Rank")
 st.write("---")
 
-st.write("Java Basic - Hacker Rank")
+st.subheader("Java Basic - Hacker Rank")
 st.image("images/java basic.jpg", caption="Hacker Rank")
 st.write("---")
 
-st.write("SQL Basic - Hacker Rank")
+st.subheader("SQL Basic - Hacker Rank")
 st.image("images/sql basic.jpg", caption="Hacker Rank")
 st.write("---")
 
-st.write("Problem Solving Basic - Hacker Rank")
+st.subheader("Problem Solving Basic - Hacker Rank")
 st.image("images/problem_solving_basic (Hacker Rank).png", caption="Hacker Rank")
 st.write("---")
 
-st.write("AI for Everyone - Maharatech")
+st.subheader("AI for Everyone - Maharatech")
 st.image("images/AI for Everyone - ITI - maharatech en.jpg", caption="Maharatech")
 st.write("---")
 
-st.write("Python Programming Basics - Maharatech")
+st.subheader("Python Programming Basics - Maharatech")
 st.image("images/Python Programming Basics Mahara Tech-1.jpg", caption="Maharatech")
 st.write("---")
 
-st.write("Introduction to Java - Sololearn")
+st.subheader("Introduction to Java - Sololearn")
 st.image("images/Introduction to Java_certificate.jpg", caption="Sololearn")
 st.write("---")
 
-st.write("Introduction to Python - Sololearn")
+st.subheader("Introduction to Python - Sololearn")
 st.image("images/Introduction to Python_certificate.jpg", caption="Sololearn")
 st.write("---")
 
-st.write("Python Intermediate - Sololearn")
+st.subheader("Python Intermediate - Sololearn")
 st.image("images/Python Intermediate_certificate.jpg", caption="Sololearn")
 st.write("---")
 
-st.write("Python Developer - Sololearn")
+st.subheader("Python Developer - Sololearn")
 st.image("images/Python Developer_certificate.jpg", caption="Sololearn")
 st.write("---")
 
-st.write("Introduction to Programming using Python - Tawar We Ghayaer - Ministry of Youth and Sports - Ministry of Telecommunication - Microsoft")
+st.subheader("Introduction to Programming using Python - Tawar We Ghayaer - Ministry of Youth and Sports - Ministry of Telecommunication - Microsoft")
 st.image("images/python 1.jpg", caption="Tawar We Ghayar")
 st.write("---")
 
-st.write("Python Programming Language Intermediate Level - Tawar We Ghayaer - Ministry of Youth and Sports - Ministry of Telecommunication - Microsoft")
+st.subheader("Python Programming Language Intermediate Level - Tawar We Ghayaer - Ministry of Youth and Sports - Ministry of Telecommunication - Microsoft")
 st.image("images/python 2.jpg", caption="Tawar We Ghayar")
 st.write("---")
 
-st.write("Introduction to Python Programming - Edraak")
+st.subheader("Introduction to Python Programming - Edraak")
 st.image("images/download (4).jpg", caption="Edraak")
 st.write("---")
 
-st.write("Java Programming 1 - Edraak")
+st.subheader("Java Programming 1 - Edraak")
 st.image("images/java 1.jpg", caption="Edraak")
 st.write("---")
 
-st.write("Java Programming 2 - Edraak")
+st.subheader("Java Programming 2 - Edraak")
 st.image("images/java 2.jpg", caption="Edraak")
 st.write("---")
 
-st.write("Java Programming 3 - Edraak")
+st.subheader("Java Programming 3 - Edraak")
 st.image("images/java 3.jpg", caption="Edraak")
 st.write("---")
 
-st.write("Python - Kaggle")
+st.subheader("Python - Kaggle")
 st.image("images/Abdallah Fekry Mohammed - Python.png", caption="Kaggle")
 st.write("---")
 
-st.write("Java Programming - Great Learning")
+st.subheader("Java Programming - Great Learning")
 st.image("images/Abdallah_Fekry_Mohammed20221019-2047-1pathg3.jpg", caption="Great Learning")
 st.write("---")
 
-st.write("Digital Literacy Training Program - Egyptian Ministry of Youth and Sports")
+st.subheader("Digital Literacy Training Program - Egyptian Ministry of Youth and Sports")
 # st.image("images/", caption="")
 st.write("---")
 
-st.write("Data Analysis & SQL Training Program - Egyptian Ministry of Youth and Sports")
+st.subheader("Data Analysis & SQL Training Program - Egyptian Ministry of Youth and Sports")
 # st.image("images/ caption="Great Learning")
 st.write("---")
 
-st.write("Freelancing Training Program - Egyptian Ministry of Youth and Sports")
+st.subheader("Freelancing Training Program - Egyptian Ministry of Youth and Sports")
 # st.image("images/", caption="Great Learning")
 st.write("---")

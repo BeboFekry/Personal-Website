d7cd3f4a78b3c6afbddfd5cea0bf06af1ad070b0
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-07 15:56:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.logo("images/Picsart_24-07-24_23-25-40-729.png")
 
-intro = st.Page("intro.py", title="Preview", icon=":material/slideshow:")
-home = st.Page("home.py", title="Home", icon=":material/home:", default=True)
+intro = st.Page("intro.py", title="Preview", icon=":material/slideshow:", default=True)
+home = st.Page("home.py", title="Home", icon=":material/home:")
 profile = st.Page("profile.py", title="Profile", icon=":material/person:")
 certificates = st.Page("certificates.py", title="Certificates", icon=":material/workspace_premium:")
 projects = st.Page("projects.py", title="Projects", icon=':material/share:')

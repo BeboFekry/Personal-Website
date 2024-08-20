1e511e5bb74a11f0091e7cb4b8e6b794577df575
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-21 01:24:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.logo("images/Picsart_24-07-24_23-25-40-729.png")
 
+st.set_page_config(page_title="Abdallah", page_icon='images/ai (2).png')
+
 if "first_time" not in st.session_state:
   st.session_state.first_time = True
 

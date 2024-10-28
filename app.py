a4d73bf8e489b04f2df7c3904a42efc2173e1baa
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-10-28 19:27:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ st.set_page_config(page_title="Abdallah", page_icon='images/ai (2).png')
 if "first_time" not in st.session_state:
   st.session_state.first_time = True
 
-intro = st.Page("intro.py", title="Preview", icon=":material/slideshow:", default=True)
-home = st.Page("home.py", title="Home", icon=":material/home:")
+# intro = st.Page("intro.py", title="Preview", icon=":material/slideshow:", default=True)
+home = st.Page("home.py", title="Home", icon=":material/home:", default=True)
 profile = st.Page("profile.py", title="Profile", icon=":material/person:")
 certificates = st.Page("certificates.py", title="Certificates", icon=":material/workspace_premium:")
 projects = st.Page("projects.py", title="Projects", icon=':material/share:')
 
-pg = st.navigation([intro, profile, home, certificates, projects])
+pg = st.navigation([profile, home, certificates, projects])
 pg.run()

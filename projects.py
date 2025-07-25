48c1f8148b03f9e54f4a798d00bf8a039fccab75
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2025-07-25 08:11:49 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -223,8 +223,14 @@ st.link_button("Kaggle Notebook Link", "https://www.kaggle.com/code/bebofekry/ge
 st.divider()
 
 st.info("23. Nasa Hazardous Objects Detection ")
-# st.write("""explain""")
+st.write("""* The mission to detect the hazardous objects in these case the Recall is so important much more than the accuracy because we can to allow with 
+misclassification the the non hazardous objects (FP) but predicting that the hazardous objects as they aren't (FN) that may makes disasters
+
+* **Conclusion**: by removing the outliers, missing data, correlated features in the cleaning phase, and by using the oversamplig and downsampling teqniques together 
+with Random Forest Model it gives the best accuracy with Testing Recall 97.5%.
+
+These model is capable of identify the hazardous objects by 97.5% wich is that main task and these means that it is doing its job perfectly""")
 st.columns([1,5,1])[1].image("images/Nasa-Hazardous-Objects-Detection.jpeg")
 st.link_button("GitHub Repository To See Implementation Notebooks", "https://github.com/BeboFekry/Nasa-Hazardous-Objects-Detection/tree/main", use_container_width=1)
-# st.link_button("Kaggle Notebook Link", "https://www.kaggle.com/code/bebofekry/gender-classification", use_container_width=1)
+st.link_button("Kaggle Notebook Link", "https://www.kaggle.com/code/bebofekry/nasa-hazardous-objects-detection", use_container_width=1)
 st.divider()

f29a28d137c9c7e8e5e664271a9573291293f462
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2025-07-25 23:28:31 +0300

    Update home.py

--- home.py
+++ home.py
@@ -331,6 +331,28 @@ st.write("Arabic (native)")
 st.write("English (very good)")
 st.write("---")
 
+st.info("Publications")
+st.warning("🏅 Winner of "Best Distinguished Applied Solution Showcase" in "Computer Vision Projects Expo 2024 Competition" by Ready Tensor publication website.")
+st.write("""#### I Care - Smart Doctor
+The project consists of three main modules:
+
+Module 1: An AI based smart chatbot called "Caroline" talking to the patient and taking its disease symptoms by text or voice messages, then diagnosing the disease and recommend making some tests or medical imaging scans to do as x-ray, MRI, Complete Blood Count CBC, ... in addition, given information about the predicted disease as an overview, symptoms, and treatments. 
+
+It can predict 30 diseases such as (Breast Cancer, Influenza, Covid 19, Stroke, ...) 
+
+Module 2: A sequence of AI Computer Vision models for scanning medical imaging scans and medical tests it can scan (X-ray, MRI, CT, OCT, CBC, or Food images), detect the image type (Image Recognition), if it is medical imaging image, applying anatomical recognition, disease evaluation, disease diagnosis, and also in tumor or bone fraction cases it can segmentate the tumor or the fraction using image segmentation. 
+
+It can predict 25 disease types such as (Bone Fracture, Brain Tumor, Covid 19, Breast Cancer, ...) 
+
+It can read the Complete Blood Count (CBC) test images and evaluate overall health and diagnose conditions like anemia, infections, clotting disorders, and blood cancers by analyzing red and white blood cells, hemoglobin, hematocrit, and platelets. 
+
+It also can recognize 101 food types from images and shows the approximate number of calories per gram. 
+
+Module 3: An ensemble Machine Learning (Random Forest) Model for scan Electrocardiography ECG and diagnosis the heart diseases. 
+
+The AI module is designed in different parts. There are a Natural Language Processing NLP, Deep Learning Computer Vision Classification, Image Segmentation, Optical Character Recognition OCR, Large Language Model LLM, Speech Recognition, and Machine Learning Models all are combined together to mimic a doctor for all specialties.""")
+st.write("---")
+
 st.info("Projects")
 projects = st.Page("projects.py")
 if st.button("View All Projects", use_container_width=1):

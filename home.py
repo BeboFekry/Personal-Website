45845c2ac62a70e6662e3d40fa0968e9d1d8c885
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2026-04-29 11:30:05 +0300

    Update home.py

--- home.py
+++ home.py
@@ -45,30 +45,17 @@ st.divider()
 
 # Overview
 st.header(":material/overview: Overview", divider='blue')
-text_to_justify = """As a graduate in Computer Science by grade Very Good with Honors with a solid foundation in machine learning, NLP, computer vision, and full-cycle AI deployment, I bring not only technical proficiency but also a passion for using AI to solve real-world problems with more than +35 projects portfolio.
+st.markdown("""As a graduate in Computer Science by grade Very Good with Honors with a solid foundation in machine learning, NLP, computer vision, and full-cycle AI deployment, I bring not only technical proficiency but also a passion for using AI to solve real-world problems with more than +35 projects portfolio.
 
-During my academic career, I led a graduation project that integrated NLP and computer vision to develop a smart medical chatbot capable of diagnosing 30 diseases and analyzing medical images using 14 deep learning models with over 95% accuracy. 
-This hands-on experience sharpened my ability to manage AI projects end-to-end from ideation and model development to deployment via Flask APIs and Streamlit.
+                During my academic career, I led a graduation project that integrated NLP and computer vision to develop a smart medical chatbot capable of diagnosing 30 diseases and analyzing medical images using 14 deep learning models with over 95% accuracy. 
+                This hands-on experience sharpened my ability to manage AI projects end-to-end from ideation and model development to deployment via Flask APIs and Streamlit.
 
-In addition, I completed multiple internships in AI and machine learning, where I worked on over 15+ practical projects involving Data Analysis, Visualization, Dashboards, LLM and Smart Agents with LangChain and RAG, OCR-based invoices processing, lip-syncing with Wav2Lip, and advanced computer vision tasks. 
-These experiences taught me to work in Agile teams and individually, communicate effectively, and rapidly prototype solutions that meet real business needs.
+                In addition, I completed multiple internships in AI and machine learning, where I worked on over 15+ practical projects involving Data Analysis, Visualization, Dashboards, LLM and Smart Agents with LangChain and RAG, OCR-based invoices processing, lip-syncing with Wav2Lip, and advanced computer vision tasks. 
+                These experiences taught me to work in Agile teams and individually, communicate effectively, and rapidly prototype solutions that meet real business needs.
 
-What sets me apart is my ability to bridge the gap between data preprocessing, modeling and deployment, transforming models into production-ready tools. 
-I am also constantly learning, holding several certifications in Python, Problem Solving, SQL, Data Analysis, Machine Learning, Data Science, and AI from respected institutions like Huawei ICT Academy, Microsoft Student Club, and NTI."""
-st.markdown(
-f"""
-<div style='text-align: justify; 
-            background-color: #ffffff; 
-            padding: 15px; 
-            border-radius: 10px; 
-            font-size: 16px; 
-            line-height: 1.6;
-            color: #333;'>
-        {text_to_justify}
-        </div>
-""",
-unsafe_allow_html=True
-)
+                What sets me apart is my ability to bridge the gap between data preprocessing, modeling and deployment, transforming models into production-ready tools. 
+                I am also constantly learning, holding several certifications in Python, Problem Solving, SQL, Data Analysis, Machine Learning, Data Science, and AI from respected institutions like Huawei ICT Academy, Microsoft Student Club, and NTI."""
+            ,text_alignment='justify')
 st.divider()
 
 profile = st.Page("profile.py", title="Profile", icon=":material/person:")
